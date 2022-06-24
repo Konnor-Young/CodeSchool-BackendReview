9302dbc0760a2ed13d0b5200745d08a2312105b8
Konnor-Young <konnor.young93@gmail.com>
2022-06-24 10:25:19 -0600

    socket for global chat working

--- server.js
+++ server.js
@@ -6,7 +6,19 @@ const { Server } = require('socket.io');
 const io = new Server(server);
 const Post = require('./database/model');
 
-
+app.get('/global', (req, res)=>{
+    res.sendFile(__dirname+"/chat.html");
+});
+io.on('connection', (socket)=>{
+    console.log("a new user connected");
+    socket.on('chat message', (msg)=>{
+        console.log(msg);
+        io.emit('chat message', msg);
+    })
+    socket.on('disconnect', ()=>{
+        console.log("user disconnected");
+    })
+});
 
 module.exports = {
     server: server
